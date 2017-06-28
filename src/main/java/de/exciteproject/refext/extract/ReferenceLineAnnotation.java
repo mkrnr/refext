f587fb5b46062bb6d2010f419beaa8c9cbbe7abf
Hosseini, Azam <azam.hosseini@gesis.org>
2017-06-28 14:23:34 +0200

    Add get method for annotations to ReferenceLineAnnotation

--- src/main/java/de/exciteproject/refext/extract/ReferenceLineAnnotation.java
+++ src/main/java/de/exciteproject/refext/extract/ReferenceLineAnnotation.java
@@ -8,31 +8,35 @@ import java.util.Map;
  * {@link ReferenceLineAnnotator}.
  */
 public class ReferenceLineAnnotation {
-    private String line;
-    private Map<String, Double> annotations;
+	private String line;
+	private Map<String, Double> annotations;
 
-    public ReferenceLineAnnotation(String line) {
-        this.line = line;
-        this.annotations = new HashMap<String, Double>();
-    }
+	public ReferenceLineAnnotation(String line) {
+		this.line = line;
+		this.annotations = new HashMap<String, Double>();
+	}
 
-    public void addAnnotation(String label, Double value) {
-        this.annotations.put(label, value);
-    }
+	public void addAnnotation(String label, Double value) {
+		this.annotations.put(label, value);
+	}
 
-    public String getBestAnnotation() {
-        Map.Entry<String, Double> maxEntry = null;
+	public String getBestAnnotation() {
+		Map.Entry<String, Double> maxEntry = null;
 
-        // from: https://stackoverflow.com/a/5911199/2174538
-        for (Map.Entry<String, Double> entry : this.annotations.entrySet()) {
-            if ((maxEntry == null) || (entry.getValue().compareTo(maxEntry.getValue()) > 0)) {
-                maxEntry = entry;
-            }
-        }
-        return maxEntry.getKey();
-    }
+		// from: https://stackoverflow.com/a/5911199/2174538
+		for (Map.Entry<String, Double> entry : this.annotations.entrySet()) {
+			if ((maxEntry == null) || (entry.getValue().compareTo(maxEntry.getValue()) > 0)) {
+				maxEntry = entry;
+			}
+		}
+		return maxEntry.getKey();
+	}
 
-    public String getLine() {
-        return this.line;
-    }
+	public Map<String, Double> getAnnotations() {
+		return this.annotations;
+	}
+
+	public String getLine() {
+		return this.line;
+	}
 }
