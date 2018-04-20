4482b9096b41d38242cbefc2ca1dfe301659737c
Hosseini, Azam <azam.hosseini@gesis.org>
2018-04-20 10:12:04 +0200

    adding main function to ReferenceLineMerger.java

--- src/main/java/de/exciteproject/refext/extract/ReferenceLineMerger.java
+++ src/main/java/de/exciteproject/refext/extract/ReferenceLineMerger.java
@@ -1,9 +1,23 @@
 package de.exciteproject.refext.extract;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FilenameUtils;
+
+import de.exciteproject.refext.ReferenceExtractor;
+import de.exciteproject.refext.extract.ReferenceLineAnnotation;
+import de.exciteproject.refext.util.FileUtils;
+import pl.edu.icm.cermine.exception.AnalysisException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import pl.edu.icm.cermine.tools.CharacterUtils;
+import pl.edu.icm.cermine.exception.AnalysisException;
 
 /**
  * Class for extracting and merging references lines from a list of BIO tagged
@@ -13,6 +27,36 @@ public class ReferenceLineMerger {
 
     private final static String hyphenList = String.valueOf(CharacterUtils.DASH_CHARS).replaceAll("-", "") + "-";
 
+    public static void main(String[] args) throws AnalysisException, IOException {
+        File inputDir = new File(args[0]);
+        File outputDir = new File(args[1]);
+        if (!outputDir.exists()) {
+            outputDir.mkdirs();
+        }
+
+        List<File> inputFiles = FileUtils.asList(inputDir);
+        for (File inputFile : inputFiles) {
+            String subDirectories = inputFile.getParentFile().getAbsolutePath().replace("\\", "/")
+                    .replaceFirst(inputDir.getAbsolutePath().replace("\\", "/"), "");
+            //File currentOutputDirectory = new File(outputDir.getAbsolutePath() + File.separator + subDirectories);
+            File currentOutputDirectory = new File(outputDir.getAbsolutePath());
+            String outputFileName = FilenameUtils.removeExtension(inputFile.getName()) + ".csv";
+            File outputFile = new File(currentOutputDirectory + File.separator + outputFileName);
+
+            // skip computation if outputFile already exists
+            if (outputFile.exists()) {
+                continue;
+            }
+            if (!currentOutputDirectory.exists()) {
+                currentOutputDirectory.mkdirs();
+            }
+            // Read Line
+            List<String> list_of_references = org.apache.commons.io.FileUtils.readLines(inputFile, Charset.defaultCharset());
+            List<String> refs_list_merged = ReferenceLineMerger.merge(list_of_references);
+            Files.write(Paths.get(outputFile.getAbsolutePath()), refs_list_merged, Charset.defaultCharset());
+        }
+    }
+    
     public static List<String> merge(List<String> inputLines) {
         List<String> references = new ArrayList<String>();
 
